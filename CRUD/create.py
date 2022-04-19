5db8855d121fcfd065fecadaf77c08a5bdf7b45f
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2022-04-19 18:48:13 -0300

    create and update GRAVADORA

--- CRUD/create.py
+++ CRUD/create.py
@@ -1,21 +1,21 @@
 
 import mysql.connector
 
-SPOTIFY_FINAL = mysql.connector.connect(
+connector = mysql.connector.connect(
   user="root",
   password="",
   database="SPOTIFY_FINAL"
 )
 
-cursor = SPOTIFY_FINAL.cursor()
+cursor = connector.cursor()
 
 sql = "insert into GRAVADORA(nomeGravadora, bioGravadora, dataGravadora, statusGravadora, paisGravadora) values (%s, %s, %s, %s, %s)"
 data = ("Bertelsmann Music Group (BMG)", "Bertelsmann Music Group (abreviado como BMG) foi uma das seis divisões da empresa alemã Bertelsmann, formada em 1987 para englobar as atividades relacionadas às gravações musicais da empresa.", "2008-10-01", "ativa", "Estados Unidos da América")
 
 cursor.execute(sql, data)
-SPOTIFY_FINAL.commit()
+connector.commit()
 
 cursor.close()
-SPOTIFY_FINAL.close()
+connector.close()
 
-print(SPOTIFY_FINAL)
+print(connector)
