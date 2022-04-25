20d41f0fe6f46fddb81c980d3e29deba8b23bda6
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2022-04-25 11:57:58 -0300

    add mudanças usando o GUI e tentando utilizar o blob

--- camadaPersistencia/main.py
+++ camadaPersistencia/main.py
@@ -6,6 +6,8 @@ from DAO import PodcastDAO, EpisodioDAO, GeneroDAO
 from runDAO import RunPodcast, RunEpisodio, RunGenero
 
 header ="\n |\/|  o   _  ._   _     |\/|  o   _  ._   _    \n |  |  |  (_  |   (_)    |  |  |  (_  |   (_)   \n\n  __                      _     \n (_   ._    _   _|_  o  _|_      \n __)  |_)  (_)   |_  |   |   \/   \n      |                      /    \n"
+
+
 def crudInterface():
     print("\n\nOlá, este é o Micro Micro Spotify, aqui podemos consultar, inserir, deletar e atualizar dados do nosso banco de dados.\nNo momento você gostaria de utilizar qual classe do banco de Dados? Escolha entre as disponíveis abaixo:\n(1)   Álbum\n(2)   Artista\n(3)   Concerto\n(4)   Episódio\n(5)   Gênero\n(6)   Gravadora\n(7)   Música\n(8)   Playlist\n(9)   Podcast\n(10)   Usuário\n(0) SAIR DO PROGRAMA\n(digite somente o número da classe escolhida)")
     value = input("ESCREVA O NÚMERO DA CLASSE ESCOLHIDA:  ")
